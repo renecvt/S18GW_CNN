0858a4f90753eeeba40d1364e44ef22fc4424904
Karen González <karenggv19@gmail.com>
2018-06-26 09:47:34 -0500

    GW_Data

--- GW_Data.py
+++ GW_Data.py
@@ -1,13 +1,23 @@
-import pycbc, os
+import pycbc
+import os
 import pycbc.frame
 from pycbc.frame import losc
 from os import listdir
-from pycbc.strain import StrainBuffer
 
-mypath = '%s/data_24hrs/' % os.getcwd()
-files = listdir(mypath)
+h1_ts = []
+l1_ts = []
 
-for f in files:
-    location = mypath + f
-    channel =  '%s1:LOSC-STRAIN' % f[0]
-    ts = pycbc.frame.read_frame(location, channel)
+def read_files():
+    mypath = '%s/data_24hrs/' % os.getcwd()
+    files = listdir(mypath)
+
+    for f in files:
+        location = mypath + f
+        channel = '%s1:LOSC-STRAIN' % f[0]
+        ts = pycbc.frame.read_frame(location, channel)
+        if f[0] == 'H':
+            h1_ts.append(ts)
+        else:
+            l1_ts.append(ts)
+
+read_files()
