7c8a8175a2284514c77913df6f69d68257f9c7a9
René Vidríales Trujillo <rene.vidriales@seistreinta.com>
2018-07-03 11:14:19 -0500

    Modificado template generator y agregado masses generator

--- template_generator_v2.py
+++ template_generator_v2.py
@@ -1,16 +1,25 @@
 import os
 import numpy
 from pycbc.waveform import get_td_waveform
+from masses_generator import masses_generator
 
-default_approximant = 'SEOBNRv3_opt'
-masses = [36]
+DEFAULT_APPROXIMANT = 'SEOBNRv3_opt'
+MASSES = masses_generator()
 
 def template_generator(approximant, masses):
+    counter = 1
+    file_path = '%s/txts/dataset.txt' % os.getcwd()
+    file = open(file_path, 'at')
     for mass in masses:
-        plus_polarization, _ = get_td_waveform(approximant=approximant, mass1=mass, mass2=mass - 5, delta_t=1.0 / 4096, f_lower=20)
-        save_path = '%s/txts/' % os.getcwd()
-        file_name = '{}_{}.txt'.format(approximant, mass)
-        numpy.savetxt('{}{}'.format(save_path, file_name), plus_polarization, newline=" ")
+        plus_polarization, _ = get_td_waveform(approximant = approximant, mass1 = mass[0], mass2 = mass[1], delta_t = 1.0 / 4096, f_lower = 20)
+        plus_polarization.resize(4096)
+        plus_polarization = numpy.asarray(plus_polarization)
+        plus_polarization = plus_polarization.tolist()
+        plus_polarization = " ".join(str(pl) for pl in plus_polarization)
+        print('Writing line number %s' % counter)
+        file.write("%r\n" % plus_polarization)
+        counter += 1
+    file.close()
 
 
-template_generator(approximant = default_approximant, masses = masses)
+template_generator(approximant = DEFAULT_APPROXIMANT, masses = MASSES)
