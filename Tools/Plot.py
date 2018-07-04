0c777900105b4aacc5a3a563e43f3bc497a068e2
Karen González <karenggv19@gmail.com>
2018-07-04 10:45:35 -0500

    Formatter

--- Tools/Plot.py
+++ Tools/Plot.py
@@ -7,12 +7,13 @@ from os.path import dirname, abspath
 DIRNAME = dirname(dirname(abspath(__file__)))
 DATA = genfromtxt('%s/Files/info.csv' % DIRNAME, delimiter=',')
 
-M1= DATA[:,0]
-M2= DATA[:,1]
-MTOTAL= DATA[:,2]
-DURATION = DATA[:,3]
-DURATION1 = DATA[:,4]
-DURATION2 = DATA[:,5]
+M1 = DATA[:, 0]
+M2 = DATA[:, 1]
+MTOTAL = DATA[:, 2]
+DURATION = DATA[:, 3]
+DURATION1 = DATA[:, 4]
+DURATION2 = DATA[:, 5]
+
 
 def masses():
     plt.scatter(M1, M2)
@@ -22,6 +23,7 @@ def masses():
     plt.savefig('%s/Saved_files/saved_plots/masses.png' % DIRNAME)
     plt.show()
 
+
 def duration():
     plt.scatter(MTOTAL, DURATION)
     plt.xlabel('M-Total')
@@ -29,12 +31,14 @@ def duration():
     plt.savefig('%s/Saved_files/saved_plots/duration.png' % DIRNAME)
     plt.show()
 
+
 def duration1():
     plt.scatter(MTOTAL, DURATION1)
     plt.xlabel('M-Total')
     plt.ylabel('Duration 1')
     plt.savefig('%s/Saved_files/saved_plots/duration1.png' % DIRNAME)
     plt.show()
+
 
 def duration2():
     plt.scatter(MTOTAL, DURATION2)
