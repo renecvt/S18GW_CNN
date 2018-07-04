e3128e48ae94fcc5eba5a3e8132ae038ab5ca88f
Karen González <karenggv19@gmail.com>
2018-07-04 13:58:54 -0500

    Plots

--- template_generator.py
+++ template_generator.py
@@ -1,16 +1,20 @@
-import os
-import numpy
-from Tools.Tools import getBiggerValue
 import csv
-import Tools
+import os
+from os.path import abspath, dirname
+
+import numpy
+import pylab
 from pycbc.waveform import get_td_waveform
-import Tools.Plot as plt 
+
+import Tools
+
 from Tools.masses_generator import masses_generator
-from os.path import dirname, abspath
+from Tools.Tools import getBiggerValue
 
 DEFAULT_APPROXIMANT = 'SEOBNRv3_opt'
 MASSES = masses_generator()
 data = []
+
 
 def create_folder(folder_name, file_name):
     working_dir = os.getcwd()
@@ -28,22 +32,30 @@ def create_folder(folder_name, file_name):
         open(complete_directory, 'w').close()
     return complete_directory
 
+
 def template_generator(approximant, masses):
     counter = 1
     directory = create_folder('Files', 'dataset.txt')
     csv_directory = create_folder('Files', 'info.csv')
     file = open(directory, 'at')
     csv_file = open(csv_directory, 'w')
-    field_names = ['mass_one', 'mass_two', 'total_mass', 'duration', 'duration_one', 'duration_two']
-    csv_writer = csv.DictWriter(csv_file, fieldnames = field_names)
+    field_names = ['mass_one', 'mass_two', 'total_mass',
+                   'duration', 'duration_one', 'duration_two']
+    csv_writer = csv.DictWriter(csv_file, fieldnames=field_names)
     for mass in masses:
         info = {}
-        plus_polarization, _ = get_td_waveform(approximant = approximant, mass1 = mass[0], mass2 = mass[1], delta_t = 1.0 / 4096, f_lower = 20)
+        plus_polarization, _ = get_td_waveform(
+            approximant=approximant, mass1=mass[0], mass2=mass[1], delta_t=1.0 / 4096, f_lower=20)
         duration = plus_polarization.duration
         total_mass = mass[0] + mass[1]
         index = getBiggerValue(list(plus_polarization))
         first_part = plus_polarization[:index]
         second_part = plus_polarization[index:]
+
+        # pylab.plot(plus_polarization.sample_times,plus_polarization,  label= '')
+        # pylab.savefig('%s_%s_masses.png' % (mass[0], mass[1]))
+        # pylab.show()
+
         plus_polarization = " ".join(str(pl) for pl in plus_polarization)
         print('Writing line number %s' % counter)
         file.write("%r\n" % plus_polarization)
@@ -60,8 +72,4 @@ def template_generator(approximant, masses):
     csv_file.close()
 
 
-template_generator(approximant = DEFAULT_APPROXIMANT, masses = MASSES)
-plt.masses()
-plt.duration()
-plt.duration1()
-plt.duration2()
+template_generator(approximant=DEFAULT_APPROXIMANT, masses=MASSES)
