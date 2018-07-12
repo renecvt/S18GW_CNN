cefd4becc4469c6dbc5be7d9648dc9dfe2508031
Karen González <karenggv19@gmail.com>
2018-07-12 13:16:08 -0500

    Initial cnn

--- template_generator.py
+++ template_generator.py
@@ -87,68 +87,72 @@ def noise_template_generator():
         h1_strain = h1_arr[i]
         l1_strain = l1_arr[i]
 
-        for template in templates:
+        for index, template in enumerate(templates):
             t = list(template)
             t = TimeSeries(t, DELTA_T, epoch=l1_strain._epoch)
             t = t/10
 
             from pycbc.filter import highpass_fir, lowpass_fir
 
-            pylab.subplot(3, 1, 1)
-            pylab.plot(t.sample_times, t)
+            # pylab.subplot(3, 1, 1)
+            # pylab.plot(t.sample_times, t)
 
             t = highpass_fir(t, 40, 40)
-            pylab.subplot(3, 1, 2)
-            pylab.plot(t.sample_times, t)
+            # pylab.subplot(3, 1, 2)
+            # pylab.plot(t.sample_times, t)
 
             t = lowpass_fir(t, 40, 40)
-            pylab.subplot(3, 1, 3)
-            pylab.plot(t.sample_times, t)
+            # pylab.subplot(3, 1, 3)
+            # pylab.plot(t.sample_times, t)
 
-            pylab.show()
+            # pylab.show()
 
             h1 = h1_strain + t
             l1 = l1_strain + t
 
             for i, ifo in enumerate([h1, l1]):
-                strain = h1_strain if i == 0 else l1_strain
+                # strain = h1_strain if i == 0 else l1_strain
                 # ifo_whiten = ifo.whiten(1, 1, remove_corrupted=False)
-                p = 1 if i == 0 else 2
-                # label = 'H1' if i == 0 else 'L1'
+                # p = 1 if i == 0 else 2
+                label = 'H1' if i == 0 else 'L1'
 
-                pylab.subplot(5, 2, p)
-                pylab.plot(strain.sample_times, strain)
-                pylab.gca().axes.get_xaxis().set_visible(False)
-                pylab.gca().axes.get_yaxis().set_visible(False)
-                # pylab.ylabel('Signal %s' % label)
-                # pylab.xlabel('Time (s)')
+                # pylab.subplot(5, 2, p)
+                # pylab.plot(strain.sample_times, strain)
+                # pylab.gca().axes.get_xaxis().set_visible(False)
+                # pylab.gca().axes.get_yaxis().set_visible(False)
+                # # pylab.ylabel('Signal %s' % label)
+                # # pylab.xlabel('Time (s)')
 
-                p += 2
-                pylab.subplot(5, 2, p)
-                pylab.plot(t.sample_times, t)
-                pylab.gca().axes.get_xaxis().set_visible(False)
-                pylab.gca().axes.get_yaxis().set_visible(False)
+                # p += 2
+                # pylab.subplot(5, 2, p)
+                # pylab.plot(t.sample_times, t)
+                # pylab.gca().axes.get_xaxis().set_visible(False)
+                # pylab.gca().axes.get_yaxis().set_visible(False)
 
-                p += 2
-                pylab.subplot(5, 2, p)
-                pylab.plot(ifo.sample_times, ifo)
-                pylab.gca().axes.get_xaxis().set_visible(False)
-                pylab.gca().axes.get_yaxis().set_visible(False)
+                # p += 2
+                # pylab.subplot(5, 2, p)
+                # pylab.plot(ifo.sample_times, ifo)
+                # pylab.gca().axes.get_xaxis().set_visible(False)
+                # pylab.gca().axes.get_yaxis().set_visible(False)
 
                 times, f, qplane = ifo.qtransform(.001, logfsteps=100,
                                                   qrange=(8, 8),
                                                   frange=(20, 512))
 
-                p += 2
-                pylab.subplot(5, 2, p)
+                # p += 2
+                # pylab.subplot(5, 2, p)
+                pylab.figure(figsize=(33, 17), dpi=1, frameon=False)
                 pylab.pcolormesh(times, f, qplane**0.5, vmin=1, vmax=6)
                 pylab.yscale('log')
                 pylab.gca().axes.get_xaxis().set_visible(False)
                 pylab.gca().axes.get_yaxis().set_visible(False)
+                pylab.tight_layout()
+                pylab.savefig('CNN/training_data/GW/%s-%s.png' % (label, index), bbox_inches = 'tight', pad_inches = 0, dpi = 1)
+                pylab.close()
                 # pylab.xlabel('Time (s)')
                 # pylab.ylabel('Frequency (Hz)')
 
-            pylab.show()
+            # pylab.show()
 
             # h1_f = " ".join(str(hs) for hs in h1)
             # l1_f = " ".join(str(ls) for ls in l1)
