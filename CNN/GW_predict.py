417507c041bac2e2a9dbf668943f3593fc2a5c3d
Karen González <karenggv19@gmail.com>
2018-07-16 12:22:49 -0500

    CNN

--- CNN/GW_predict.py
+++ CNN/GW_predict.py
@@ -9,22 +9,23 @@ import tensorflow as tf
 
 # First, pass the path of the image
 dir_path = os.path.dirname(os.path.realpath(__file__))
-image_path = sys.argv[1]
+# image_path = sys.argv[1]
+image_path = '/Users/karenggv/Desktop/Projects/Delfin/S18GW_CNN/CNN/training_data/GW/H1-0.png'
 # filename = dir_path + '/' + image_path
 image_width = 32
-image_heigth = 16
+image_height = 16
 num_channels = 3
 images = []
 # Reading the image using OpenCV
 image = cv2.imread(image_path)
 # Resizing the image to our desired size and preprocessing will be done exactly as done during training
-image = cv2.resize(image, (image_width, image_heigth), 0, 0, cv2.INTER_LINEAR)
+image = cv2.resize(image, (image_width, image_height), 0, 0, cv2.INTER_LINEAR)
 images.append(image)
 images = np.array(images, dtype=np.uint8)
 images = images.astype('float32')
 images = np.multiply(images, 1.0/255.0)
 # The input to the network is of shape [None image_size image_size num_channels]. Hence we reshape.
-x_batch = images.reshape(1, image_width, image_heigth, num_channels)
+x_batch = images.reshape(1,  image_height, image_width, num_channels)
 
 # Let us restore the saved model
 sess = tf.Session()
@@ -43,11 +44,13 @@ y_pred = graph.get_tensor_by_name("y_pred:0")
 # Let's feed the images to the input placeholders
 x = graph.get_tensor_by_name("x:0")
 y_true = graph.get_tensor_by_name("y_true:0")
-y_test_images = np.zeros((1, len(os.listdir('CNN/testing_data'))))
 
+
+y_test_images = np.zeros((1, 2))
+# len(os.listdir('CNN/testing_data')
 
 # Creating the feed_dict that is required to be fed to calculate y_pred
-feed_dict_testing = {x: x_batch, y_true: y_test_images}
+feed_dict_testing = {x: x_batch}
 result = sess.run(y_pred, feed_dict=feed_dict_testing)
 # result is of this format [probabiliy_of_rose probability_of_sunflower]
 print(result)
